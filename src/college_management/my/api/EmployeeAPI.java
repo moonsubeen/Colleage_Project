5832bd0d8997ac4e2ece73da30c9ec611e22ee0f
moonsubeen <zx04090@naver.com>
2020-12-19 15:50:14 +0900

    select, update, insert 쿼리문 추가

--- src/college_management/my/api/EmployeeAPI.java
+++ src/college_management/my/api/EmployeeAPI.java
@@ -1,8 +1,47 @@
 package college_management.my.api;
 
-public class EmployeeAPI {
+import javax.persistence.EntityManagerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import college_management.my.model.Student;
+import college_management.my.model.User;
+
+public class EmployeeAPI {	
+	private static final String PERSISTENCE_UNIT_NAME = "h2";
+	private static EntityManagerFactory factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+	
 	public String studentInfo() // 학생 정보 입력
-	{
+	{	
+		EntityManager em = factory.createEntityManager();
+		User user = new User();
+		
+		EntityTransaction transaction = em.getTransaction(); // 트랜잭션은 작업단위
+	    transaction.begin(); 
+	    
+	    user.setId("");
+	    user.getPwd();
+	    user.setName("");
+	    user.setEmail("");
+	    user.setNationality("");
+	    user.setPhoneNumber("");
+	    user.setAddress("");
+	    user.setResidentNumber("");
+	    user.setBirthdate("");
+	    user.setSex("");
+	    
+	    em.persist(user);
+	    
+	    transaction.commit();
+	    em.close();
+	    
 		return null;
 	}
 	
--- src/college_management/my/api/UserAPI.java
+++ src/college_management/my/api/UserAPI.java
@@ -15,29 +15,51 @@ import college_management.my.model.User;
 public class UserAPI {
 	private static final String PERSISTENCE_UNIT_NAME = "h2";
 	private static EntityManagerFactory factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);;
-	private EntityManager em = factory.createEntityManager();
 	
 	public int login(String id, String pwd) // 로그인
 	{
 		//selcet
+		EntityManager em = factory.createEntityManager();
 		Query query = em.createQuery("select t from User t where id = " + id + "and pwd = " + pwd);
         Object resultList = query.getSingleResult();
+        em.close();
+        
         if(resultList.equals(id) && resultList.equals(pwd))
         	return 0;
         else
         	return -1;
 	}
 	
-	public String pwdUpdate(String pwd) // 패스워드 변경 
+	public int pwdUpdate(String pwd) // 패스워드 변경 
 	{
 		//update
-		Query query2 = em.createQuery("update User set pwd = ");
-        Object resultList2 = query2.getSingleResult();
-		return null;
+		EntityManager em = factory.createEntityManager();
+		EntityTransaction transaction = em.getTransaction(); // 트랜잭션은 작업단위
+		if(pwd.length() != 8)
+		{
+			pwd = "12345asd";
+		}
+		
+        transaction.begin(); 
+		Query query = em.createQuery("update User t set t.pwd = " + pwd);
+        Object resultList = query.getSingleResult();
+        em.persist(resultList);
+        transaction.commit();
+    
+        em.close();
+        
+		return 1;
 	}
 	
 	public String info() // 기본 정보 출력
 	{
+		EntityManager em = factory.createEntityManager();
+		Query query = em.createQuery("select t from User t");
+		List<User> resultList = query.getResultList();
+		for (User result : resultList) {
+//            System.out.println(result.toString());
+        }
+		em.close();
 		return null;
 	}
 	
