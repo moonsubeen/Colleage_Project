52051451b973e2c364208fc877bd73984a2fa521
moonsubeen <zx04090@naver.com>
2020-12-26 14:56:58 +0900

    간격

--- src/college_management/my/api/EmployeeAPI.java
+++ src/college_management/my/api/EmployeeAPI.java
@@ -19,30 +19,27 @@ public class EmployeeAPI {
 	private static EntityManagerFactory factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
 	
 	public static void studentInfo(String id, String name, String email, String nationality, String phoneNumber, String address, String residentNumber, String birthdate, String sex) // 학생 정보 입력
-	{	
+	{
 		EntityManager em = factory.createEntityManager();
 		User user = new User();
-			
+		
 		EntityTransaction transaction = em.getTransaction(); // 트랜잭션은 작업단위
 		transaction.begin();
+		user.setId(id);
+		user.getPwd();
+		user.setName(name);
+		user.setEmail(email);
+		user.setNationality(nationality);
+		user.setPhoneNumber(phoneNumber);
+		user.setAddress(address);
+		user.setResidentNumber(residentNumber);
+		user.setBirthdate(birthdate);
+		user.setSex(sex);
 		
-	    user.setId(id);
-	    user.getPwd();
-	    user.setName(name);
-	    user.setEmail(email);
-	    user.setNationality(nationality);
-	    user.setPhoneNumber(phoneNumber);
-	    user.setAddress(address);
-	    user.setResidentNumber(residentNumber);
-	    user.setBirthdate(birthdate);
-	    user.setSex(sex);
-	    
-	    em.persist(user);
-	    
-	    transaction.commit();
-	    em.close();
-	    
-
+		em.persist(user);
+		
+		transaction.commit();
+		em.close();
 	}
 	
 	public static void professorInfo() // 교수 정보 입력
