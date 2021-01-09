a69f131a8026650a81adf9a2fa86a465e9c36e53
moonsubeen <zx04090@naver.com>
2021-01-09 15:25:57 +0900

    login으로 인한 업데이트

--- src/college_management/my/cli/CLIInfoUpdate.java
+++ src/college_management/my/cli/CLIInfoUpdate.java
@@ -1,6 +1,7 @@
 package college_management.my.cli;
 
 import college_management.my.api.UserAPI;
+import college_management.my.model.User;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
@@ -8,11 +9,11 @@ import picocli.CommandLine.ParentCommand;
 
 @Command(name = "infoupdate")
 public class CLIInfoUpdate implements Runnable {
-	@Parameters(paramLabel = "Role", description = "The role")
-	private String role;
-
-	@Option(names = { "-i", "--id" }, description = "The student ID")
-	private String id;
+//	@Parameters(paramLabel = "Role", description = "The role")
+//	private String role;
+//
+//	@Option(names = { "-i", "--id" }, description = "The student ID")
+//	private String id;
 
 	@Option(names = { "-e", "--email" }, description = "The student email", required = false)
 	private String email;
@@ -27,20 +28,24 @@ public class CLIInfoUpdate implements Runnable {
 	CliCommands parent;
 
 	public void run() {
-		switch (role) {
+		CLIAuth auth = CLIAuth.getInstance();
+		if(!auth.isLogin())
+		{
+			parent.out.println("It's need to login");
+			return;
+		}
+		
+		User user = CLIAuth.getInstance().getUser();
+		
+		switch (user.getRole()) {
 		case "student":
-			if (id == "")
-				break;
 			UserAPI api = new UserAPI();
-			boolean result = api.infoUpdate(id, email, address, phoneNumber);
+			boolean result = api.infoUpdate(user.getId(), email, address, phoneNumber);
 			if (result) {
 				parent.out.println("update success");
 			} else {
 				parent.out.println("update fail");
 			}
-			break;
-		default:
-			parent.out.printf("'%s' is not support\n", role);
 			break;
 		}
 	}
